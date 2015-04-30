b9a7d3e15a3c7e2e270308058e63b1915210e834
Martin Droge <mdroge@ds-creative.com>
2015-04-30 10:23:32 -0400

    Got the "Help" button popover working on all existing pages, added styles for the popover bubble and added the javascript that lets it run.

---
+++ scripts/popover.js
@@ -0,0 +1,11 @@
+// Show pop-over
+function showPopOver(divID) {
+    //Show the div
+    document.getElementById(divID).style.display="block";
+}
+
+//Close pop-over
+function closePopOver(divID) {
+    //Hide the div
+    document.getElementById(divID).style.display="none";
+}
